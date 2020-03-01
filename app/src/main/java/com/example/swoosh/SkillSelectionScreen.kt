db93d2f8a1d03ad9375fe08ea9a2c031262cfc81
aravindmedamoni <aravnd.accurate@gmail.com>
2020-03-01 16:54:16 +0530

    ui designed for skill selection screen..

---
+++ app/src/main/java/com/example/swoosh/SkillSelectionScreen.kt
@@ -0,0 +1,11 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillSelectionScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill_selection_screen)
+    }
+}
