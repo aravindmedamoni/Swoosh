0f85285fd005df2a180790ff5e87fdfa7545ee6a
aravindmedamoni <aravnd.accurate@gmail.com>
2020-03-01 20:00:51 +0530

    functionality is added to the all the buttons of the LeagueActivity.kt..

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -6,11 +6,35 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
+
+    var selectedLeague = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        menToggleButton.setOnClickListener {
+            womenToggleButton.isChecked = false
+            coedToggleButton.isChecked = false
+            selectedLeague = "Men"
+        }
+
+        womenToggleButton.setOnClickListener {
+            menToggleButton.isChecked = false
+            coedToggleButton.isChecked = false
+            selectedLeague = "Women"
+        }
+
+        coedToggleButton.setOnClickListener {
+            menToggleButton.isChecked = false
+            womenToggleButton.isChecked = false
+            selectedLeague = "Co-ed"
+        }
         nextButton.setOnClickListener {
-            startActivity(Intent(this,SkillSelectionScreen::class.java))
+            if(selectedLeague != ""){
+                val skillIntent = Intent(this,SkillSelectionScreen::class.java)
+                skillIntent.putExtra(EXTRA_LEAGUE,skillIntent)
+                startActivity(skillIntent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/swoosh/SkillSelectionScreen.kt
+++ app/src/main/java/com/example/swoosh/SkillSelectionScreen.kt
@@ -1,11 +1,16 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SkillSelectionScreen : AppCompatActivity() {
+
+    var league = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill_selection_screen)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+
     }
 }
